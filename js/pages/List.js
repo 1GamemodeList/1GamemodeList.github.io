3c40608748a9a9671fb2f38705dd83bd6a004b72
ValidashGD <sl.parkour@outlook.com>
2025-07-27 09:35:05 +0100

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -110,6 +110,7 @@ export default {
                     </p>
                     <p>
                         The creator should preferably involve themselves more than once in the server and inactivity will inevitably lead to the removal of the creators levels.
+                    <p>    
                     <h3>Completion Requirements</h3>
                     <p>
                         To submit an record, you need an completion with clicks. You may use a mod that makes click sounds, however we would greatly appreciate if you have your mic on.
